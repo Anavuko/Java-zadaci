e5c898f85c3b3c3501bc49008ce256fbaf83804a
Anavuko <45428706+Anavuko@users.noreply.github.com>
2018-12-12 16:36:55 +0100

    Predavanje 2 Zadatak 2

---
+++ Pred2Zad2.java
@@ -0,0 +1,29 @@
+package Predavanje2;
+
+import java.util.Scanner;
+
+public class Pred2Zad2 {
+
+	public static void main(String[] args) {
+		int n;
+		String max_ime = "";
+		int max_ocena = -1;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Unesite broj studenata: ");
+		n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(i + 1 + ": Unesite ime i ocenu studenta: ");
+			String ime = sc.next();
+			int ocena = sc.nextInt();
+			if (max_ocena < ocena) {
+				max_ocena = ocena;
+				max_ime = ime;
+			}
+		}
+
+		System.out.println("Ime studenta sa najvecom ocenom: " + max_ime + "(" + max_ocena + ")");
+		sc.close();
+	}
+}
