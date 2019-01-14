3d6612972c6f2be0f295808114a17f1fee86ce26
Anavuko <45428706+Anavuko@users.noreply.github.com>
2019-01-14 15:31:06 +0100

    Predavanje5Zadatak1

--- Main.java
+++ Main.java
@@ -1,35 +1,75 @@
-package predavanje4.zad1;
+package Predavanje5Zad1;
 
-
-import java.util.HashMap;
-
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-	static Firma firma = new Firma("Java", 500000);
-
 	public static void main(String[] args) {
-	System.out.println(firma);
+	
 		
-		Proizvod proizvod1 = new MlecniProizvodi("kravica", 135, "07.11.2019", "kravlje");
-		Proizvod proizvod2 = new Meso("but", 469, "30.12.2018", "svinjetina");
-		Proizvod proizvod3 = new Kafe("grand", 356, "08.2.2020", VrstaKafe.MLEVENA);
-		Proizvod proizvod4 = new Sokovi("next", 160, "05.05.2019", "jabuka");
-
-		System.out.println("Spisak proizvoda sa kolicinama");
-
-		firma.lista = new HashMap<>();
-		firma.lista.put(proizvod1, 55);
-		firma.lista.put(proizvod2, 85);
-		firma.lista.put(proizvod3, 155);
-		firma.lista.put(proizvod4, 505);
-
-		firma.stampanjeListaProizvoda();
-		firma.porudzbina(proizvod1,12);
-		System.out.println("Spisak proizvoda sa kolicinama nakon porudzbine" );
-		firma.stampanjeListaProizvoda();
-		System.out.println(firma);
+		Scanner sc = new Scanner(System.in);
+	
+		List<Student> listaStudenata = new ArrayList<Student>();
+	
+		while (true) {
+			Student student = new Student();
+			System.out.println(" Unesite ime i prezime studenta: ");
+			String line = sc.nextLine();
+			if (line.equals("Exit")) {
+				
+			    break;
+			   
+			}else {
+				System.out.println(line);
+				student.setIme(line.substring(0, line.indexOf(" ")));
+				student.setPrezime(line.substring(line.indexOf(" ")+1));
+				
+			}
+			System.out.println("Unesi broj poena: ");
+			line = sc.nextLine();
+			if (line.equals("Exit")) {
+				
+			    break;
+			   
+			}else {
+				try {
+					int poeni = Integer.parseInt(line);
+					student.setBrojPoena(poeni);
+					listaStudenata.add(student);
+					
+				}catch(Exception e){ 
+					System.out.println("Greska nije unet broj " + line);
+					
+					
+				}
+				System.out.println(line);
+				
+			}
+		}
+		for (Student s:listaStudenata) {
+			System.out.println(s);
+		}
+		Collections.sort(listaStudenata);
+		System.out.println("Studenti nakon sortiranja ");
+	
+		sc.close();
+		PrintWriter izlaz;
+		try {
+			izlaz = new PrintWriter (new FileWriter("ocene.txt"));
+			for (Student s:listaStudenata) {
+				System.out.println(s);
+				izlaz.println(s);
+			}	
+			izlaz.close();
+		}catch (IOException e) {
+			
+		}
+	
+		
 	}
 	
 }
